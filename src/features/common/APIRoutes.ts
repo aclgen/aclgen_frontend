9c941d9732582e632b785855e13159e856d1209d
sigmundgranaas <sigmundsgranaas@gmail.com>
2022-04-27 14:12:34 +0200

    Created new route for production

--- src/features/common/APIRoutes.ts
+++ src/features/common/APIRoutes.ts
@@ -1,9 +1,20 @@
-export const host: string = "http://localhost";
+import { env } from "process";
+
+export const host: string = "http://localhost:8000";
+
+
+export const realhost: string = "https://api.aclgen.com";
 
 export const port: string = ":8000";
 
 export const baseRoute: string = "/api/";
 
 export function createAPIRoute(route: string) {
-  return `${host}${port}${baseRoute}${route}`;
+  if(env.NODE_ENV == "development"){
+    return `${host}${baseRoute}${route}`;
+  
+  }else if(env.NODE_ENV == "production"){
+    return `${realhost}${baseRoute}${route}`;
+  }
 }
+  
